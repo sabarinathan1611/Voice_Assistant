a7ae4e40d8b75ec05c382209a1148a84c7d91002
v.sabarinathan <97294218+sabarinathan1611@users.noreply.github.com>
2024-01-09 14:00:16 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+a='open youtube'
+b='hello'
+c='hello noob'
+
+while True :
+    if 'open youtube' in a:
+        print(a)
+    for b=="hello" :
+        for b in range (1):
+            print(suess)
+     
